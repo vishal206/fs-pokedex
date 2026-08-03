e8a365a8e904dc49550490a54215316b09359bf4
vishal206 <vishal206ani@gmail.com>
2026-08-03 20:18:13 +0530

    feat:playwrite test

---
+++ e2e-tests/pokedex.spec.js
@@ -0,0 +1,15 @@
+const { test, describe, expect } = require('@playwright/test')
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('a pokemon page can be navigated to', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
+})
--- eslint.config.js
+++ eslint.config.js
@@ -15,7 +15,7 @@ module.exports = [
   },
   js.configs.recommended,
   {
-    files: ['app.js', 'jest.setup.js'],
+    files: ['app.js', 'jest.setup.js', 'playwright.config.js', 'e2e-tests/**/*.js'],
     languageOptions: {
       ecmaVersion: 2018,
       sourceType: 'commonjs',
